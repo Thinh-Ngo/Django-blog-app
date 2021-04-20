a006440faa95c8e1c737fb1bee515accdbe7c9fb
Thinh Ngo <thanhthinhkrb@gmail.com>
2021-04-20 17:21:58 +0700

    resting

--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(
-        default="https://www.w3schools.com/howto/img_avatar.png",
+        default="default.jpg",
         upload_to="profile_pics",
     )
 
